f5d928dca1b5bf682df05ac898c15bee625b1802
MorganShorter <MorganShorter@users.noreply.github.com>
2015-09-03 18:18:22 +0700

    Got rid of extra variable declarations and edited comments for accuracy.

--- dust-this.js
+++ dust-this.js
@@ -2,12 +2,9 @@
 
 var dust = require('dustjs-linkedin');
 var fs = require('fs');
-//var templateFile = process.argv[2];
 var keyFile = process.argv[2];
-//var outputFile = process.argv[4];
-var c = console.log;
 
-// pain free error catching
+// Pain free error catching.
 var e = function (err) {
   if (err) {
     throw err;
@@ -18,9 +15,8 @@ var e = function (err) {
 dust.config.whitespace = true;
 
 /*
- * Takes in the file system paths (relative and absolute) of a dust template 
- * file, a key file containing a JSON object (and nothing else), and an output 
- * file; then processes the template and key files into an output file.
+ * Takes in a key file containing a JSON object (and nothing else), 
+ * then processes the key file and template(s) into an output file.
  * 
  * This overwrites any existing data in the output file without warning. BE CAREFUL!
  */
@@ -33,20 +29,20 @@ var dustThis = function (keyFile) {
        * NOTE: There is no exception handling. These templates MUST compile for 
        * things to work
        */
+      var compiled;
       if (keys.partials) {
 	for (var i = 0; i < keys.partials.length; i++) {
-	  var compiled = dust.compile(fs.readFileSync(keys.partials[i]).toString(), keys.partials[i]);
+	  compiled = dust.compile(fs.readFileSync(keys.partials[i]).toString(), keys.partials[i]);
 	  dust.loadSource(compiled);
 	}
       }
       // Compile and load the main template
-      var compiled = dust.compile(fs.readFileSync(keys.template).toString(), keys.template);
+      compiled = dust.compile(fs.readFileSync(keys.template).toString(), keys.template);
       dust.loadSource(compiled);
       // Render the main template and write it to a file
       dust.render(keys.template, keys, function(err, out) {
 	  e(err);
 	  fs.writeFile(keys.output_file, out);	
-	  //c(out);
       });   
   });
 }
